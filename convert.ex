92bd05d3c3ebc51944f290b7a005f315becb7111
Spike Grobstein <spike@ticketevolution.com>
2013-07-25 21:06:02 -0400

    removed extra indent.

--- convert.ex
+++ convert.ex
@@ -1,15 +1,16 @@
 defmodule Math do
-    def pow( num, power ) do
-      do_pow num, power, 1
-    end
 
-    defp do_pow( _num, 0, acc ) do
-      acc
-    end
+  def pow( num, power ) do
+    do_pow num, power, 1
+  end
 
-    defp do_pow( num, power, acc ) when power > 0 do
-      do_pow( num, power - 1, acc * num)
-    end
+  defp do_pow( _num, 0, acc ) do
+    acc
+  end
+
+  defp do_pow( num, power, acc ) when power > 0 do
+    do_pow( num, power - 1, acc * num)
+  end
   
 end
 
